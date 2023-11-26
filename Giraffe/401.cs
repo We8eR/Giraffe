bd0c4918ff1cd186f40fe2715213c3d2cda3c2b2
We8eR <122805348+We8eR@users.noreply.github.com>
2023-11-26 20:41:03 +0300

    /

---
+++ Giraffe/401.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security;
+using System.Runtime.InteropServices;
+
+public static class Program401
+{
+    public static void Main()
+    {
+        using (SecureString ss = new SecureString())
+        {
+            Console.Write("Please enter password: ");
+            while (true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Enter) break;
+                ss.AppendChar(cki.KeyChar);
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            DisplaySecureString(ss);
+        }
+    }
+    private unsafe static void DisplaySecureString(SecureString ss)
+    {
+        Char* pc = null;
+        try
+        {
+            pc = (Char*)Marshal.SecureStringToCoTaskMemUnicode(ss);
+            for (Int32 index = 0; pc[index] != 0; index++)
+                Console.Write(pc[index]);
+        }
+        finally
+        {
+            if (pc != null)
+                Marshal.ZeroFreeCoTaskMemUnicode((IntPtr)pc);
+
+        }
+    }
+}
---
+++ Giraffe/421.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class _421
+    {
+        FileStream[,] fs2dim = new FileStream[5, 10];
+        Object[,] o2dim = fs2dim;
+        Stream[] s1dim = (Stream[,]) o2dim;
+        String[,] s1dim = (String[,]) o2dim;
+        String[,] s2dim = (String[,]) o2dim;
+        String[,] st2dim = (String[,]) o2dim;
+        Int32[] ildim = new int32[5];
+        Object[] oldim = (Object[,]) o2dim;
+        Object[] obldim = new Object[ildim.Length];
+        Array.Copy(ildim, obldim, ildim.Lenght);
+    }
+}
