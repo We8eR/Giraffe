e0ff08a67af168828116400c1b2a96f8201f9f0e
We8eR <122805348+We8eR@users.noreply.github.com>
2023-12-17 22:43:01 +0300

    /

---
+++ Giraffe/457.cs
@@ -0,0 +1,21 @@
+﻿internal sealed class AClass
+{
+    public static void UsingLocalVariablesInTheCallbackCode(Int32 numToDo)
+    {
+        Int32[] squares = new Int32[numToDo];
+        AutoResetEvent done = new AutoResetEvent(false);
+
+        for (Int32 n = 0; n < squares.Length; n++)
+        {
+            ThreadPool.QueueUserWorkItem(obj =>
+            {
+                Int32 num = (Int32)obj;
+                squares[num] = num * num;
+                if (Interlocked.Decrement(ref numToDo) == 0) done.Set();
+            }, n);
+        }
+        done.WaitOne();
+        for (Int32 n = 0; n < squares.Length; n++)
+            Console.WriteLine("Index {0}, Square - {1}", n, squares[n]);    
+    }
+}
---
+++ Giraffe/458.cs
@@ -0,0 +1,39 @@
+﻿internal sealed class AClass
+{
+    public static void UsingLocalVariablesInTheCallbackCode(Int32 numToDo)
+    {
+        WaitCallback callback1 = null;
+
+        <> c_DisplayClass2 class1 = new <> c_DisplayClass2();
+
+        class1.numToDo = numToDo;
+        class1.squares = new Int32[class1.numToDo];
+        class1.done = new AutoResetEvent(false);
+
+        for (Int32 n = 0; n < class1.sqares.Length; n++)
+        {
+            if (callback1 == null)
+            {
+                callback1 = new WaitCallback(class1.< UsingLocalVariablesInTheCallbackCode > b_0);
+
+            }
+            ThreadPool.QueueUserWorkItem(callback1, n);
+        }
+        class1.done.WaitOne();
+        for (Int32 n = 0; n < class1.squares.Length; n++)
+            Console.WriteLine("Index {0}, Square={1}", n, class1.squares[n]);
+    }
+    private sealed class <>c_DisplayClass2: Object{
+        public Int32[] squares;
+    public Int32 numToDo;
+    public AutoResetEvent done;
+    public <>c_DisplayClass2{ }
+    public void <UsingLocalVariablesInTheCallbackCode>b_0(Object obj){
+        Int32 num = (Int32) obj;
+        squares[num] = num * num;
+    if (Interlocked.Decrement(ref numToDo) == 0)
+        done.Set();
+
+}
+}
+}
