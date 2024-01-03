799861580a42e0e55d577b59e722693cfc974d85
We8eR <122805348+We8eR@users.noreply.github.com>
2024-01-03 14:47:22 +0300

    .

---
+++ Giraffe/652.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using Wintellect.HostSDk;
+
+public static class Program
+{
+    public static void Main()
+    {
+        String AddInDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly.Location);
+        var AddInAssemblies = Directory.EnumerateFiles(AddInDir, "*.dll");
+        var AddInTypes = from file in AddInAssemblies let assembly = Assembly.Load(file)
+        from t in assembly.ExportedTypes
+        where t.IsClass && typeof(IAddIn).GetTypeInfo().IsAssignableFrom(t.GetTypeInfo())
+        select t;
+    foreach (Type t in AddInTypes)
+        {
+            IAddIn ai = (IAddIn) Avtivator.CreateInstance(t);
+            Console.WriteLine(ai.ToString(5));
+        }
+    }
+}
+
